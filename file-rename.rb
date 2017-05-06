faa236d8a72247d1d6eb7654499293dac0e2a36e
Samuel Mak <samuelswmak@gmail.com>
2017-05-06 16:48:49 -0700

    file rename works

--- file-rename.rb
+++ file-rename.rb
@@ -5,8 +5,13 @@ count = 0
 
 folder_path = "./rename/"
 Dir.glob(folder_path + "*").sort.each do |f|
-  # filename = File.basename(f, File.extname(f))
-  # File.rename(f, folder_path + filename.capitalize + File.extname(f))
+  filename = File.basename(f, File.extname(f))
+  if filename.include?("-")
+    slice_idx = filename.index("-")
+    new_filename = filename[(slice_idx + 1)..(filename.length)]
+    puts new_filename
+    count += 1
+  end
 end
 
 puts "Renaming complete. #{count} files renamed."
