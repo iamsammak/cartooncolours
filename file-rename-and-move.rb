c4e655671a9b2f28e9e4500976382f45211bafbb
Samuel Mak <samuelswmak@gmail.com>
2017-05-08 02:02:51 -0700

    add user input logic

--- file-rename-and-move.rb
+++ file-rename-and-move.rb
@@ -6,7 +6,9 @@ def begin_renaming
   # folder_path = "./rename/"
   puts "Enter the folder path. ex. './folder_path/' "
   folder_path = gets.chomp
-  puts folder_path
+  puts "Is this the correct folder path: `#{folder_path}`?"
+  correct = gets.chomp
+  return if correct == "no"
 
   Dir.glob(folder_path + "*").sort.each do |f|
     filename = File.basename(f, File.extname(f))
@@ -18,8 +20,8 @@ def begin_renaming
     elsif new_filename.length == 2
       new_filename = "0" + new_filename
     end
-    # File.rename(f, folder_path + new_filename + File.extname(f))
-    puts new_filename
+
+    File.rename(f, folder_path + new_filename + File.extname(f))
     count += 1
   end
 
@@ -28,6 +30,35 @@ end
 
 def begin_move
   puts "Beginning move"
+  current_folder_path = ""
+  new_folder_path = ""
+
+  loop do
+    puts "What is the current folder path?"
+    current_folder_path = gets.chomp
+    # ./rename/
+
+    puts "Is this correct `#{current_folder_path}` ?"
+    first_check = gets.chomp
+    next if first_check == "no"
+
+    puts "What will be the new folder path?"
+    new_folder_path = gets.chomp
+    # ./images/master-svg/
+
+    puts "Is this correct `#{new_folder_path}` ?"
+    second_check = gets.chomp
+    next if second_check == "no"
+
+    Dir.glob(current_folder_path + "*").sort.each do |f|
+      filename = File.basename(f)
+      File.rename(f, new_folder_path + filename)
+    end
+
+    break
+  end
+
+  puts "Move complete. Files moved from `#{current_folder_path}` to `#{new_folder_path}`"
 end
 
 def start
