96d0d7ccdb7d701f33790ba6df196ec887ca2264
Samuel Mak <samuelswmak@gmail.com>
2017-05-08 02:23:46 -0700

    appending pokemon name to svg file

--- file_test.rb
+++ file_test.rb
@@ -1,14 +1,13 @@
 # testing rename files
-folder_path = './rename/'
-Dir.glob(folder_path + "*").sort.each do |f|
-  filename = File.basename(f, File.extname(f))
-  puts filename
-  # slice_index = filename.index("_")
-  # puts filename[(slice_index + 1)..(filename.length)]
-  slice_index = filename.index("-")
-  puts filename[0...slice_index]
-end
-
+# folder_path = './rename/'
+# Dir.glob(folder_path + "*").sort.each do |f|
+#   filename = File.basename(f, File.extname(f))
+#   puts filename
+#   # slice_index = filename.index("_")
+#   # puts filename[(slice_index + 1)..(filename.length)]
+#   slice_index = filename.index("-")
+#   puts filename[0...slice_index]
+# end
 
 # move files root folder into rename folder
 # Dir.glob("./" + "*").sort.each do |f|
@@ -17,3 +16,13 @@ end
 #     File.rename(f, "./rename/" + f)
 #   end
 # end
+
+# appending pokemon name to svg file names
+require_relative 'pokemon_list' # OR require './pokemon_list'
+
+folder_path = "./images/master-svg/"
+Dir.glob(folder_path + "*").sort.each do |f|
+  filename = File.basename(f, File.extname(f))
+
+  puts filename
+end
